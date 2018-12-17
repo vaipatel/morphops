f57d45622333a5bfe145cf664c47d1bbb745f49b
Vaibhav Patel <vai.pateln@gmail.com>
2018-12-16 23:31:22 -0500

    Write some opa tests

--- tests/test_procrustesalgos.py
+++ tests/test_procrustesalgos.py
@@ -18,6 +18,19 @@ class TestProcrustesAlgos(object):
                    (haus0_refl, haus0, True, haus0_refl_al, False,
                     "when X = YT, no_reflect, T refl, with non-0 ssq")
                    ]
+    
+    opa_data = [(haus, haus0_rot, False, True,
+                 haus0_rot, 1, haus0_Ro, -np.dot(haus_c, haus0_Ro), 0, 0,
+                 "when X = YT + c, T rot, no_reflect, with 0 oss."),
+                (haus, haus0_refl, False, False,
+                 haus0_refl, 1, haus0_Rf, -np.dot(haus_c, haus0_Rf), 0, 0,
+                 "when X = YT + c, T refl, with 0 oss."),
+                (make_ngon(10)*3, make_ngon(10), False, False,
+                 make_ngon(10)*3, 1, np.identity(2), 0, 10*np.square(3-1), 3+1,
+                 "when X = bY, with 10*(b-1)^2 oss, 1 oss_stdized."),
+                 (make_ngon(10)*3, make_ngon(10), True, False,
+                 make_ngon(10), 1/3, np.identity(2), 0, 0, 0,
+                 "when X = bY, do_scaling, with 0 oss.")]
 
     @pytest.mark.parametrize("source, target, no_reflect, src_rot, "
                              "should_match_target, scn",
@@ -29,3 +42,16 @@ class TestProcrustesAlgos(object):
         assert np.allclose(rot_res['src_rot'], src_rot)
         assert np.allclose(rot_res['src_rot'], target) == should_match_target
 
+    @pytest.mark.parametrize("source, target, do_scaling, no_reflect, src_rot, "
+                             "b, T, c, oss, oss_stdized, scn",
+                             opa_data)
+    def test_opa(self, source, target, do_scaling, no_reflect, src_rot,
+                 b, T, c, oss, oss_stdized, scn):
+        print("opa should min ||Y - bXR + 1*c.T||^2 -", scn)
+        opa_res = procrustes.opa(source, target, do_scaling, no_reflect)
+        assert np.allclose(opa_res["src_rot"], src_rot)
+        assert np.allclose(opa_res["b"], b)
+        assert np.allclose(opa_res["R"], T)
+        assert np.allclose(opa_res["c"], c)
+        assert np.allclose(opa_res["oss"], oss)
+        assert np.allclose(opa_res["oss_stdized"], oss_stdized)
