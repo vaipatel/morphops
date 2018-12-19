f63ab27ea31744aa45e0090d6dc1024d94b083ef
Vaibhav Patel <vai.pateln@gmail.com>
2018-12-19 08:49:49 -0500

    Write helper to the number of landmark sets.

--- morphops/lmk_util.py
+++ morphops/lmk_util.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+def num_lmk_sets(X):
+    if (len(np.shape(X)) is not 3):
+        raise ValueError("The input X must be a 3d tensor corresponding to a "
+                         "list of landmark sets.")
+    return np.shape(X)[0]
+
 def num_lmks(X):
     X_shape = np.shape(X)
     X_d_sz = len(X_shape)
