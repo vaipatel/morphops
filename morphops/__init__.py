babf99935213af298caef0afb70e037b4f3035d0
Vaibhav Patel <vai.pateln@gmail.com>
2018-12-19 09:53:52 -0500

    Update __init__.py

--- morphops/__init__.py
+++ morphops/__init__.py
@@ -1,3 +1,6 @@
 # Not sure if prepending morphops to module is necessary, but makes it easier to import into jupyter for testing.
-from .lmk_util import transpose, num_coords, num_lmks
-from .procrustes import get_position, get_scale, remove_position, remove_scale, rotate, opa
+from .lmk_util import \
+    transpose, num_coords, num_lmks, num_lmk_sets, ssqd
+from .procrustes import \
+    get_position, get_scale, remove_position, remove_scale, \
+    rotate, opa, gpa
