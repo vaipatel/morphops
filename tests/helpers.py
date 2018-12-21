cc766692564c8b0147eeabdff4a8eef50f3cce21
Vaibhav Patel <vai.pateln@gmail.com>
2018-12-21 01:38:18 -0500

    Make general 2d reflection from angle func

--- tests/helpers.py
+++ tests/helpers.py
@@ -6,9 +6,9 @@ def get_2d_rot(angle=0):
     return np.array([[np.cos(angle),-np.sin(angle)],
                     [np.sin(angle),np.cos(angle)]])
 
-def a_2d_refl():
-    return np.array([[-np.cos(piOvr4),np.sin(piOvr4)],
-                    [np.sin(piOvr4),np.cos(piOvr4)]])
+def get_2d_refl(angle=0):
+    return np.array([[-np.cos(angle),np.sin(angle)],
+                    [np.sin(angle),np.cos(angle)]])
 
 def make_ngon(nsides=6, angle_offset=0):
     ns = np.arange(0,2*np.pi, 2*np.pi/nsides) + angle_offset
@@ -27,7 +27,7 @@ def make_haus():
     two = 2
     haus0_scld = two*haus0
     haus0_Ro = get_2d_rot(piOvr4)
-    haus0_Rf = a_2d_refl()
+    haus0_Rf = get_2d_refl(piOvr4)
     haus0_rot = np.dot(haus0, haus0_Ro)
     haus0_refl = np.dot(haus0, haus0_Rf)
     haus0_refl_al = np.dot(haus0, [[-1,0],[0,1]]) # haus0 refl across y axis
