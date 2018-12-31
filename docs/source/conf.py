c84f31e84254af96d04cee15eceff49ad559a9f3
Vaibhav Patel <vai.pateln@gmail.com>
2018-12-30 22:17:24 -0500

    Bump to 0.1.7a2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ author = 'Vai Patel'
 # The short X.Y version
 version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = '0.1.7a1'
+release = '0.1.7a2'
 
 # -- General configuration ---------------------------------------------------
 
